bd1a77aa3f5659d557d2efcc8ed0ef1373d275b5
Subhamdas20 <subham21.sd@gmail.com>
2021-11-13 11:07:40 +0530

    Added : Ability to respond happy or sad

---
+++ src/test/java/com/bridgelabz/testanalyzer/TestMoodAnalyzer.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.testanalyzer;
+
+import com.brideglabz.analyser.MoodAnalyzer;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class TestMoodAnalyzer {
+    @Test
+    public void whenGivenAMessageShouldAnalyzeAndRespondSad(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am In a Sad Mood");
+        String mood = moodAnalyzer.analyseMood();
+        Assert.assertEquals("SAD", mood);
+    }
+    @org.junit.Test
+    public void whenGivenAMessageShouldAnalyzeAndRespondHappy(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am In a Any Mood");
+        String mood = moodAnalyzer.analyseMood();
+        Assert.assertEquals("HAPPY", mood);
+    }
+    @Test
+    public void whenGivenAMessageShouldAnalyzeAndRespondHAPPY(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am In a Happy Mood");
+        String mood = moodAnalyzer.analyseMood();
+        Assert.assertEquals("HAPPY", mood);
+    }
+}
